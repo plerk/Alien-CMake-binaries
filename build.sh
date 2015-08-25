ebf127a18a67695c4868118a2090436bb0c9da67
Graham Ollis <plicease@cpan.org>
2015-08-25 15:25:35 -0400

    uncompress for systems that do not do it automatically

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 VERSION=3.3.1
 
 wget http://www.cmake.org/files/v3.3/cmake-$VERSION.tar.gz
-tar xf cmake-$VERSION.tar.gz
+gzip -d cmake-$VERSION.tar.gz
+tar xf cmake-$VERSION.tar
 cd cmake-$VERSION
 cmake -DCMAKE_FIND_LIBRARY_SUFFIXES=.a -DBUILD_SHARED_LIBRARIES=OFF -DCMAKE_EXE_LINKER_FLAGS=-static -DBUILD_CursesDialog=OFF .
 make package
