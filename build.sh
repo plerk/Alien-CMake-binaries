e9d032b554c6b9a62efa219e739f9d1d8d21952d
Graham Ollis <plicease@cpan.org>
2015-08-25 14:49:38 -0400

    initial checkin

---
+++ build.sh
@@ -0,0 +1,7 @@
+VERSION=3.3.1
+
+wget http://www.cmake.org/files/v3.3/cmake-$VERSION.tar.gz
+tar xf cmake-$VERSION.tar.gz
+cd cmake-$VERSION
+cmake -DCMAKE_FIND_LIBRARY_SUFFIXES=.a -DBUILD_SHARED_LIBRARIES=OFF -DCMAKE_EXE_LINKER_FLAGS=-static -DBUILD_CursesDialog=OFF .
+make package
